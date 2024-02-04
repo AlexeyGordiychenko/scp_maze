f6430a9b9bcee63af8cb1fbb987ebab3da741b49
elidacon <elidacon@student.21-school.ru>
2024-02-04 22:18:54 +0500

    Fix View for the new UI (from previous commmit)

--- src/view/s21_view.cc
+++ src/view/s21_view.cc
@@ -7,9 +7,9 @@ s21::View::View(Controller* controller, QWidget* parent)
     : QMainWindow(parent), ui_(new Ui::View), controller_(controller) {
   ui_->setupUi(this);
 
-  connect(ui_->openFile, &QPushButton::clicked, this, &View::OpenFile);
-  connect(ui_->filePath, qOverload<int>(&QComboBox::currentIndexChanged), this,
-          &View::FilePathChange);
+  connect(ui_->mazeOpenFile, &QPushButton::clicked, this, &View::OpenFile);
+  connect(ui_->mazeFilePath, qOverload<int>(&QComboBox::currentIndexChanged),
+          this, &View::FilePathChange);
 
   ui_->mazeWidget->SetController(controller);
   ui_->caveWidget->SetController(controller);
@@ -21,21 +21,21 @@ void s21::View::OpenFile() {
   QString filename = QFileDialog::getOpenFileName(
       this, tr("Open maze file:"), "~/", tr("Maze Files (*.txt)"));
   if (filename.isEmpty()) return;
-  auto index = ui_->filePath->findText(filename);
-  auto count = ui_->filePath->count();
+  auto index = ui_->mazeFilePath->findText(filename);
+  auto count = ui_->mazeFilePath->count();
   if (index == -1) {
     if (count >= history_max_list_) {
-      ui_->filePath->removeItem(0);
+      ui_->mazeFilePath->removeItem(0);
       count--;
     }
-    ui_->filePath->addItem(filename);
+    ui_->mazeFilePath->addItem(filename);
     index = count;
   }
   FilePathChange(index);
 }
 
 void s21::View::RenderMaze() {
-  std::string filename = ui_->filePath->currentText().toStdString();
+  std::string filename = ui_->mazeFilePath->currentText().toStdString();
   try {
     controller_->Initialize(filename);
     ui_->mazeWidget->Initialize();
@@ -47,6 +47,6 @@ void s21::View::RenderMaze() {
 }
 
 void s21::View::FilePathChange(int idx) {
-  ui_->filePath->setCurrentIndex(idx);
+  ui_->mazeFilePath->setCurrentIndex(idx);
   RenderMaze();
 }
